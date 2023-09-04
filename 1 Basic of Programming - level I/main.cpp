17ff719ac58cb86330700ad1230317a3053ffb44
Sandeep Patel <sandeepatel.0101@gmail.com>
2023-09-04 13:38:44 +0530

    ...

--- 1 Basic of Programming - level I/main.cpp
+++ 1 Basic of Programming - level I/main.cpp
@@ -2,7 +2,74 @@
 using namespace std;
 int main()
 {
-    cout << "sandeep patel" << endl;
-    cout << "sandeep patel" << endl;
+
+    char ch;
+    cin >> ch;
+    cout << "here: " << ch << endl;
+
+    // int marks;
+
+    // cout << "Enter the marks here: " << endl;
+    // //take input from user
+    // cin >> marks;
+
+    // cout << "Printing marks: " << marks << endl;
+
+    // int age = 101;
+    // int car = 12;
+
+    // if(age >= 18 || car >=1) {
+    // 	cout << "License miljaega" << endl;
+    // }
+
+    // cout << !age << endl;
+
+    // int a = 20;
+    // int b = 10;
+
+    // cout << (a == b) << endl;
+
+    // int a = 3;
+    // int b = 2;
+
+    // cout << a + b << endl;
+    // cout << a - b << endl;
+    // cout << a*b << endl;
+    // cout << a/b << endl;
+    // cout << b % a << endl;
+    // garbage value
+    // int num;
+    // num = 12;
+    // cout << num << endl;
+
+    // int -> 4 byte
+    //  int num = 50;
+    //  cout << num << endl;
+
+    // cout << sizeof(num) << endl;
+
+    // //character -> 1byte
+    // char ch = 'k';
+    // cout << ch << endl;
+    // cout << sizeof(ch) << endl;
+
+    // //float -> 4 byte
+    // float point = 1.69;
+    // cout << point << endl;
+    // cout << sizeof(point) << endl;
+
+    // //long -> 4 byte
+    // long number = 23;
+    // cout << number << endl;
+    // cout << sizeof(number) << endl;
+
+    // cout << "Sandy " << endl << "Sandy" << '\n';
+    // cout << "Sandeep Patel" << endl ;
+    // cout << endl;
+    // cout << endl;
+    // cout << '\n';
+    // cout << "Sandeepa Patel" ;
+
+    // find in homework
     return 0;
 }
