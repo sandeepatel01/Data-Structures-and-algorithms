1e7bdf1b7fa00c0014b511d3a808fe31856f315b
Sandeep Patel <sandeepatel.0101@gmail.com>
2023-09-05 11:26:23 +0530

    ...

--- 3 Basic of Programming - level II/3-Functions/four.cpp
+++ 3 Basic of Programming - level II/3-Functions/four.cpp
@@ -1,7 +1,17 @@
+// Write a function to print the sum of 3 numbers
+
 #include <bits/stdc++.h>
 using namespace std;
+
+void printSum(int num1, int num2, int num3)
+{
+    int sum = num1 + num2 + num3;
+    cout << "Sum is: " << sum << endl;
+}
 int main()
 {
+
+    printSum(2, 2, 2);
 
     return 0;
 }
