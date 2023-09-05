e5466df8d9ddc35cd8801b1fcc4099d3b58641b5
Sandeep Patel <sandeepatel.0101@gmail.com>
2023-09-05 11:40:52 +0530

    ...

--- 3 Basic of Programming - level II/3-Functions/four.cpp
+++ 3 Basic of Programming - level II/3-Functions/four.cpp
@@ -8,10 +8,18 @@ void printSum(int num1, int num2, int num3)
     int sum = num1 + num2 + num3;
     cout << "Sum is: " << sum << endl;
 }
+
+void printMessage()
+{
+    cout << "msg1" << endl;
+    return;
+    cout << "msg2" << endl;
+}
 int main()
 {
 
-    printSum(2, 2, 2);
+    // printSum(2, 2, 2);
+    printMessage();
 
     return 0;
 }
