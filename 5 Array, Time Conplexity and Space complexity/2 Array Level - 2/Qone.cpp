27b27d1f1e315daf208e6f9f8419b3bb175efd94
Sandeep Patel <sandeepatel.0101@gmail.com>
2023-09-12 16:06:08 +0530

    ...

--- 5 Array, Time Conplexity and Space complexity/2 Array Level - 2/Qone.cpp
+++ 5 Array, Time Conplexity and Space complexity/2 Array Level - 2/Qone.cpp
@@ -1,3 +1,5 @@
+// Find Unique Elements 
+
 #include <bits/stdc++.h>
 using namespace std;
 
---
+++ 5 Array, Time Conplexity and Space complexity/2 Array Level - 2/Qtwo.cpp
@@ -0,0 +1,23 @@
+// Print All Pairs
+
+#include <bits/stdc++.h>
+using namespace std;
+void printPairs(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << "(" << i << "," << j << ")" << endl;
+        }
+    }
+}
+
+int main()
+{
+    int arr[] = {10, 20, 30};
+    int n = 3;
+
+    printPairs(arr, n);
+    return 0;
+}
