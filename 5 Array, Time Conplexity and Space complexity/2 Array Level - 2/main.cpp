976f22275082880eaf1253495e567493b5d4f10f
Sandeep Patel <sandeepatel.0101@gmail.com>
2023-09-12 15:56:03 +0530

    ...

--- 5 Array, Time Conplexity and Space complexity/2 Array Level - 2/main.cpp
+++ 5 Array, Time Conplexity and Space complexity/2 Array Level - 2/main.cpp
@@ -1,6 +1,22 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
- 
- return 0;
+
+int printUniqueElemens(int arr[], int n)
+{
+    int ans = 0;
+    for (int i = 0; i < n; i++)
+    {
+        ans = ans ^ arr[i];
+    }
+    return ans;
+}
+int main()
+{
+    int arr[] = {10, 2, 11, 10, 2, 13, 15, 13, 15};
+    int n = 9;
+
+    int uniqueElement = printUniqueElemens(arr, n);
+    cout << "Unique Element is: " << uniqueElement << endl;
+
+    return 0;
 }
