530b431c47fb3ee72f0a8383a3effb81813a709c
Sandeep Patel <sandeepatel.0101@gmail.com>
2023-09-13 17:44:44 +0530

    ...

---
+++ 5 Array, Time Conplexity and Space complexity/4 Vector - STL in C++/staticAllocation.cpp
@@ -0,0 +1,18 @@
+// Static Memory Allocation in Array 
+
+#include<bits/stdc++.h>
+using namespace std;
+void print(int arr[], int n){
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+}
+int main(){
+ 
+ int arr[] = {1,2,3,4,5};
+ int n=5;
+
+print(arr, n);
+
+ return 0;
+}
