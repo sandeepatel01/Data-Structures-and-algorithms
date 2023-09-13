d794a07c2000e4fb5e689b12d2788533dba29e13
Sandeep Patel <sandeepatel.0101@gmail.com>
2023-09-14 00:46:05 +0530

    ...

--- 5 Array, Time Conplexity and Space complexity/4 Vector - STL in C++/vector.cpp
+++ 5 Array, Time Conplexity and Space complexity/4 Vector - STL in C++/vector.cpp
@@ -11,20 +11,25 @@ int main(){
  
  vector<int>V;
 
- while(1){
-    int d;
-    cin>>d;
-    V.push_back(d);
-    cout<<"Capacity: "<<V.capacity()<<"size: "<<V.size()<<endl;
- }
+//  while(1){
+//     int d;
+//     cin>>d;
+//     V.push_back(d);
+//     cout<<"Capacity: "<<V.capacity()<<"size: "<<V.size()<<endl;
+//  }
 
 //  inserting 
-// V.push_back(1);
-// V.push_back(2);
-// V.push_back(3);
-// V.push_back(4);
-// V.push_back(5);
-// V.push_back(6);
+V.push_back(1);
+V.push_back(2);
+V.push_back(3);
+V.push_back(4);
+V.push_back(5);
+V.push_back(6);
+
+V.pop_back();
+V.pop_back();
+V.pop_back();
+
 
 print(V);
 
