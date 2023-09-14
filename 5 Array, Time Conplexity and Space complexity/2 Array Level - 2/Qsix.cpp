39facb87e97efacaeea860f63d16326d6b0bcfc2
Sandeep Patel <sandeepatel.0101@gmail.com>
2023-09-14 13:33:09 +0530

    ...

---
+++ 5 Array, Time Conplexity and Space complexity/2 Array Level - 2/Qsix.cpp
@@ -0,0 +1,52 @@
+// Sort`s 0`s and 1`s 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void sortZeroOne(int arr[], int n){
+    int zeroCount = 0;
+    int oneCount = 0;
+
+    // Step one - count 0 & 1 
+    for(int i=0; i<n; i++){
+        if(arr[i] == 0){
+            zeroCount++;
+        }
+        if(arr[i] == 1){
+            oneCount++;
+        }
+    }
+    // step two - place all zeros first 
+
+    // int i;
+    // for(i=0; i<zeroCount; i++){
+    //     arr[i] = 0;
+    // }
+    // for(int j=i; j<n; j++){
+    //     arr[j] = 1;
+    // }
+
+    // easy way 
+    int index = 0;
+    while(zeroCount--){
+        arr[index] = 0;
+        index++;
+    }
+    while(oneCount--){
+        arr[index] = 1;
+        index++;
+    }
+}
+int main(){
+ 
+  int arr[] = {0,1,0,1,0,1,1,1,1,0,0,0,0,0};
+  int n = 14;
+ 
+ sortZeroOne(arr, n);
+
+ for(int i=0; i<n; i++){
+    cout<<arr[i]<<" ";
+ }
+
+ return 0;
+}
