e10967946dbf0660df2c9de6dfecbf7596f0741b
Sandeep Patel <sandeepatel.0101@gmail.com>
2023-09-15 15:10:09 +0530

    ...

--- 5 Array, Time Conplexity and Space complexity/3 Arry Level - 3/main.cpp
+++ 5 Array, Time Conplexity and Space complexity/3 Arry Level - 3/main.cpp
@@ -2,5 +2,27 @@
 using namespace std;
 int main(){
  
+//  Creating 2D array 
+ int arr[3][4] = {
+    {1,2,3,4},
+    {4,5,6,5},
+    {7,8,9,6}
+ };
+
+  int arr2[][4] = {
+    {1,2,3,4},
+    {4,5,6,5},
+    {7,8,9,6}
+ };
+
+    //  show errors 
+  int arr3[3][] = {
+    {1,2,3,4},
+    {4,5,6,5},
+    {7,8,9,6}
+ };
+
+
+
  return 0;
 }
