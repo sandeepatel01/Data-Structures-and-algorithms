5ed085fff4d9d498b9f72a7190db891db17f5e41
Sandeep Patel <sandeepatel.0101@gmail.com>
2023-09-19 17:06:31 +0530

    ...

--- 5 Array, Time Conplexity and Space complexity/3 Arry Level - 3/vector.cpp
+++ 5 Array, Time Conplexity and Space complexity/3 Arry Level - 3/vector.cpp
@@ -14,7 +14,29 @@ int main(){
 // }
 
 // 2D Array 
-vector<vector<int> > arr(5, vector<int>(10,0));
+// vector<vector<int> > arr(5, vector<int>(10,0));
+
+// for(int i=0; i<arr.size(); i++){
+//     for(int j=0; j<arr[i].size(); j++){
+//         cout<<arr[i][j]<<" ";
+//     }
+//     cout<<endl;
+// }
+
+// jagged array 
+vector< vector<int> > arr;
+
+vector<int> v1(10,0);
+vector<int> v2(5,1);
+vector<int> v3(7,8);
+vector<int> v4(8,1);
+vector<int> v5(20,0);
+
+arr.push_back(v1);
+arr.push_back(v2);
+arr.push_back(v3);
+arr.push_back(v4);
+arr.push_back(v5);
 
 for(int i=0; i<arr.size(); i++){
     for(int j=0; j<arr[i].size(); j++){
