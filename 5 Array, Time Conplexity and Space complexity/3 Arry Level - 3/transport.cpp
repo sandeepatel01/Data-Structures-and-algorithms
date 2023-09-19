10f56cda64dd3cf9be9deda4186efe4d869a5377
Sandeep Patel <sandeepatel.0101@gmail.com>
2023-09-19 12:18:12 +0530

    ...

--- 5 Array, Time Conplexity and Space complexity/3 Arry Level - 3/transport.cpp
+++ 5 Array, Time Conplexity and Space complexity/3 Arry Level - 3/transport.cpp
@@ -12,8 +12,13 @@ void print2DArray(int arr[3][3], int row, int col){
  }
 }
 
-void printTransportMatrix(int arr[][], int row, int col){
-    
+void printTransportMatrix(int arr[3][3], int row, int col){
+    for(int i=0; i<row; i++){
+        for(int j=i; j<col; j++){
+            swap(arr[i][j], arr[j][i]);
+        }
+    }
+      print2DArray(arr, row, col);
 }
 
 int main(){
@@ -28,6 +33,11 @@ int main(){
  int col = 3;
 
 print2DArray(arr, row, col);
+cout<<endl;
+cout<<endl;
+
+printTransportMatrix(arr, row, col);
+
 
 
  return 0;
