749041b5ae3c84d4d39613362e9cd9f10bb9607b
Sandeep Patel <sandeepatel.0101@gmail.com>
2023-09-26 22:51:55 +0530

    bubble sort

--- 7 Searching and Sorting/5_Bubble_Sort/main.cpp
+++ 7 Searching and Sorting/5_Bubble_Sort/main.cpp
@@ -1,31 +1,54 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-void print(vector<int> &v){
-    for(int i=0; i<v.size(); i++){
-        cout<<v[i]<<" ";
+void print(vector<int> &v)
+{
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << " ";
     }
-    cout<<endl;
+    cout << endl;
 }
 
-void bubbleSort(vector<int> &v){
+void bubbleSort(vector<int> &v)
+{
     int n = v.size();
-    for(int i=0; i<n-1; i++){
-        for(int j=0; j<n-i-1; j++){
-            if(v[j] > v[j+1]){
-                swap(v[j], v[j+1]);
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (v[j] > v[j + 1])
+            {
+                swap(v[j], v[j + 1]);
             }
         }
     }
 }
 
-int main(){
- 
- vector<int> v = {5,4,3,2,1};
+// sort by decreasing order
+void bubbleSortByDecreasingOrder(vector<int> &v)
+{
+    int n = v.size();
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (v[j] < v[j + 1])
+            {
+                swap(v[j], v[j + 1]);
+            }
+        }
+    }
+}
 
- print(v);
- bubbleSort(v);
- print(v);
+int main()
+{
 
- return 0;
+    vector<int> v = {5, 4, 3, 2, 1};
+
+    print(v);
+    bubbleSort(v);
+    print(v);
+
+    return 0;
 }
