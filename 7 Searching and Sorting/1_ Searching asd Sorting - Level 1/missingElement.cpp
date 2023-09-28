fe15d4e3f7e951132d40a20bf838969a5543d7db
Sandeep Patel <sandeepatel.0101@gmail.com>
2023-09-28 22:43:04 +0530

    finding missing elements

--- 7 Searching and Sorting/1_ Searching asd Sorting - Level 1/missingElement.cpp
+++ 7 Searching and Sorting/1_ Searching asd Sorting - Level 1/missingElement.cpp
@@ -22,14 +22,16 @@ int findMissingElement(int arr[], int n){
         }
         mid = start + (end - start)/2;
     }
+    if(ans+1 == 0)
+    return n+1;
     return ans+1;
 }
 
 int main(){
  
 //  int arr[] = {1,2,3,4,6,7,8};
-int arr[] = {1,3,4,5,6,7,8};
- int n = 7;
+int arr[] = {1,2,3,4,5,6,7,8};
+ int n = 8;
 
  int missintNumber = findMissingElement(arr, n);
  cout<<"missing number is: "<<missintNumber<<endl;
