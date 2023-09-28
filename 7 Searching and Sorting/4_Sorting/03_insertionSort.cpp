2c4985661626d87c942970a88fe72ac076998e8c
Sandeep Patel <sandeepatel.0101@gmail.com>
2023-09-28 19:28:25 +0530

    ...

--- 7 Searching and Sorting/4_Sorting/03_insertionSort.cpp
+++ 7 Searching and Sorting/4_Sorting/03_insertionSort.cpp
@@ -1,6 +1,36 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+void print(vector<int> &v) {
+  for (int i = 0; i < v.size(); ++i) {
+    cout << v[i] << " ";
+  }
+  cout << endl;
+}
+
+void insertionSort(vector<int> &v) {
+  int n = v.size();
+  // i = 0, chhod deta hu
+  for (int i = 1; i < n; ++i) {
+    int key = v[i];
+    int j = i - 1;
+
+    // Move elements of arr[0..i-1] that are greater than
+    // key to one position ahead of theri current position
+    while (j >= 0 && v[j] > key) {
+      v[j + 1] = v[j];
+      j--;
+    }
+    v[j + 1] = key; // insertion
+  }
+}
+
 int main(){
  
+vector<int> v = {44,33,55,22,11};
+
+insertionSort(v);
+print(v);
+
  return 0;
 }
