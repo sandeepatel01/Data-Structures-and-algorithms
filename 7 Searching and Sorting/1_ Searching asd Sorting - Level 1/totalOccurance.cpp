26e3989d86993f0b936d5b5e20c98723b9da3e41
Sandeep Patel <sandeepatel.0101@gmail.com>
2023-09-28 21:41:47 +0530

    ...

---
+++ 7 Searching and Sorting/1_ Searching asd Sorting - Level 1/totalOccurance.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int findLasttOccurance(int arr[], int n, int target){
+    int start = 0; 
+    int end = n-1;
+    int mid = start + (end - start)/2;
+    int ans = -1;
+
+    while(start <= end){
+        if(target == arr[mid]){
+            ans = mid;    
+            start = mid+1;
+        }
+        else if(target > arr[mid]){
+            start = mid + 1;
+        }
+        else if(target < arr[mid]){
+            end = mid-1;
+        }
+        mid = start + (end - start)/2;
+    }
+    return ans;
+}
+
+int main(){
+ 
+ int arr[] = {10,20,30,30,30,30,30,50};
+ int n=8;
+ int target = 30;
+
+ int ansIndex = findLasttOccurance(arr, n, target);
+ if(ansIndex == -1){
+    cout<<"first occurance not found! "<<endl;
+ }
+ else{
+    cout<<"first occurance : "<<ansIndex<<endl;
+ }
+
+ return 0;
+}
