5fdfd17dc2307a61e6ca9ec21673908ee68cf2e4
Sandeep Patel <sandeepatel.0101@gmail.com>
2023-10-09 12:24:35 +0530

    Pointers in c++

--- 11 Basics maths and Pointers/02_Pointer Level - 2/main.cpp
+++ 11 Basics maths and Pointers/02_Pointer Level - 2/main.cpp
@@ -3,14 +3,53 @@ using namespace std;
 
 // void solve(int arr[], int size){
 void solve(int* arr, int size){
-    cout<<"size of arr in solve function: "<<sizeof(arr)<<endl;
+// 	// cout << "Size of array inside solve func: " << sizeof(arr) << endl;
+// 	// cout << "Inside solve -> arr: " << arr << endl;
+// 	// cout << "Inside solve -> &arr: " << &arr << endl;
+// 	*arr = *arr + 1;
 }
+
+void solve(int* &p) {
+	p = p + 1;
+}
+
 int main(){
  
- int arr[] = {10,20,30,40};
- cout<<"size of array in main function: "<<sizeof(arr)<<endl;
+	int a = 5;
+	int* p = &a;
 
- solve(arr, 4);
+	cout << "p: " << p << endl;
+	cout << "&p : " << &p << endl;
+	cout << "*p: " << *p << endl;
+	solve(p);
+	cout << "p: " << p << endl;
+	cout << "&p : " << &p << endl;
+	cout << "*p: " << *p << endl;
+	
+	
+
+	// int arr[] = {10,20,30};
+	// ///cout << "Size of array: " << sizeof(arr) << endl;
+	// solve(arr,3);
+
+	// for(int i=0; i<3; i++) {
+	// 	cout << arr[i] << " ";
+	// }
+	
+	// // cout << "Inside main -> arr: " << arr << endl;
+	// cout << "Inside main -> &arr: " << &arr << endl;
+
+	// int arr[5] = {1,2,3,4,5};
+
+	// //pointer to an array
+	// int* ptr1 = arr;
+	// //pointer to an array
+	// int (*ptr)[5] = &arr;
+	// cout << (*ptr)[0];
+	
+
+	// // char ch[10] = "Babbar";
+	// // char* cptr = &ch;
 
  return 0;
 }
